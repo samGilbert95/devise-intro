02d5d922eff345bfc3a51548523de4fd93ddefe7
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-10-04 11:18:34 +0100

    Model for blog working,devise implemented, Goofy and Capatin Kirk enjoy the new platform

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,6 +25,7 @@ class BlogsController < ApplicationController
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
+    @blog.user = current_user
 
     respond_to do |format|
       if @blog.save
@@ -69,6 +70,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body, :image, :user_id)
+      params.require(:blog).permit(:title, :body, :image)
     end
 end
